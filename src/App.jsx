cb304c40f66eaee4851d28a52fccb50fe6398ca1
Ashley Rarieya <ararieya@gmail.com>
2024-01-30 10:50:42 +0300

    Import of Status

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,26 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
-import Contacts from './Contacts';
+import Contacts from "./Contacts";
 import Navibar from "./Navibar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Status from "./Status";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  
   return (
     <>
-    <div id = "navbar">
-          <Navibar />
-        </div>
-    
- 
-    <Routes>
-       <Route path="/" element={<Login/>}/>
-      <Route path="/register" element= {<Signup/>}/>
-      <Route path="/contacts" element={<Contacts />} />
-     
-    </Routes>
-  
-  </>
-  )
+      <div id="navbar">
+        <Navibar />
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/status" element={<Status />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
